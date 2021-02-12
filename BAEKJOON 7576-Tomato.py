fde862fd46679eb460d27b6ba87ff8709635e3de
SooYong Kim <kst2575@naver.com>
2021-02-12 23:02:39 +0900

    21.02.12 백준 7576 토마토

--- BAEKJOON 7576-Tomato.py
+++ BAEKJOON 7576-Tomato.py
@@ -1,57 +1,38 @@
 # 백준 7576 토마토
+# deque를 한 번씩 검사했던 기존 구문이 시간초과를 야기함
+from collections import deque
+import sys
 
-def tomato(l1, node, visited):
-  i, j = node
-  tempList2 = []
-  global n, m
-  for k in range(4):
-    if 0 <= i + di[k] < n and 0 <= j + dj[k] < m:
-      if l1[i + di[k]][j + dj[k]] == 0:
-        if (i + di[k], j + dj[k]) not in visited:
-          l1[i + di[k]][j + dj[k]] = 1
-          tempList2.append((i + di[k], j + dj[k]))
-  return(tempList2)
-
+# r = sys.stdin.readline
 m, n = map(int, input().split())
 l1 = []
-visited = []
 di = [1, -1, 0, 0]
 dj = [0, 0, 1, -1]
-tempList = []
-tempList3 = []
-zero = False
 cnt = 0
-flag = False
+dq = deque()
 for i in range(n):
-  l1.append(list(map(int, input().split())))
+    tempList = list(map(int, input().split()))
+    for j in range(m):
+        if tempList[j] == 1:
+            dq.append((i, j))
+    l1.append(tempList)
+temp = -1
+while dq:
+
+    i, j = dq.popleft()
+    for k in range(4):
+        if 0 <= i + di[k] < n and 0 <= j + dj[k] < m:
+            if l1[i + di[k]][j + dj[k]] == 0:
+                l1[i + di[k]][j + dj[k]] = l1[i][j] + 1
+                temp = max(temp, l1[i][j] + 1)
+                dq.append((i + di[k], j + dj[k]))
 
 for i in range(n):
-  for j in range(m):
-    if l1[i][j] == 1:
-      visited.append((i, j))
-    elif l1[i][j] == 0:
-      zero = True
-if zero:
-  while True:
-    temp = len(visited)
-    for v in visited:
-      tempList = tomato(l1, v, visited)
-      for t in tempList:
-        tempList3.append(t)
-    for t in tempList3:
-      if t not in visited:
-        visited.append(t)
-    cnt += 1
-    for i in range(n):
-      if 0 in l1[i]:
-        if temp == len(visited):
-          print(-1)
-          flag = True
+    if 0 in l1[i]:
+        print(-1)
         break
-    else:
-      print(cnt)
-      break
-    if flag:
-      break
 else:
-  print(0)
+    if temp == -1:
+        print(0)
+    else:
+        print(temp - 1)
