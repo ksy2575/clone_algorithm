842e64692c779d63c9e5305e82ca8d12cc7a0371
SooYong Kim <kst2575@naver.com>
2021-03-30 21:32:33 +0900

    21.03.30 프로그래머스 문제 풀이

--- PROGRAMMERS/PROGRAMMERS dfs-Minimize Sales Decline.py
+++ PROGRAMMERS/PROGRAMMERS dfs-Minimize Sales Decline.py
@@ -3,34 +3,33 @@ from collections import defaultdict
 from copy import deepcopy
 import sys
 saleList = []
-minValue = 2147483648
-def do(l1, i, set1, sale):
+dp = []
+def do(l1, i, set1, pre):
     temp = 2147483648
-    global minValue
-    if sale >= minValue:
-      return -1
-    if i >= len(l1):
-      minValue = min(minValue, sale)
-      return sale
+    print(dp)
+    print(set1)
+    if pre != 0 and dp[pre] != -1:
+      return dp[pre]
     for v in l1[i]:
-      if v in set1:
-        temp =  min(temp, do(l1, i+1, set1, sale))
+      if i == len(l1)-1:
+        dp[v] = saleList[v-1]
       else:
-        tempSet = deepcopy(set1)
-        tempSet.add(v)
-        nextValue = do(l1, i+1, tempSet, sale + saleList[v-1])
-        if nextValue != -1:
-          temp =  min(temp, nextValue)
-    return temp
+        dp[v] = do(l1, i+1, set1|set([v]), v)
+      temp = min(temp, dp[v])
+    return temp+saleList[pre-1]
 
 def solution(sales, links):
     sys.setrecursionlimit(10**7)
     dic = defaultdict(set)
-    global saleList
-    saleList = deepcopy(sales)
+    global saleList, dp
+    saleList = sales
+    dp = [0] + [-1]*len(sales)
+    print(dp)
     for link in links:
       dic[link[0]].add(link[0])
       dic[link[0]].add(link[1])
+    print(sales)
+    print(dic)
 
     return do(list(dic.values()), 0, set(), 0)
 print(solution([14, 17, 15, 18, 19, 14, 13, 16, 28, 17], [[10, 8], [1, 9], [9, 7], [5, 4], [1, 5], [5, 10], [10, 6], [1, 3], [10, 2]]))
