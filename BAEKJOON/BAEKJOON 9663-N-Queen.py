a8dc56f4dabedbf685e4d79dbcf1959dd67e7129
SooYong Kim <kst2575@naver.com>
2021-04-02 00:14:07 +0900

    21.04.02 백준 백트래킹 문제 풀이

--- BAEKJOON/BAEKJOON 9663-N-Queen.py
+++ BAEKJOON/BAEKJOON 9663-N-Queen.py
@@ -1,37 +1,25 @@
-# N이 4일 때
-from copy import deepcopy
-
-
-def paint(l1, i, j):
-    for k in range(1, len(l1) + 1):
-        l1[k - 1][j] = 1
-        l1[i][k - 1] = 1
-        if 0 <= i + k < len(l1) and 0 <= j + k < len(l1):
-            l1[i + k][j + k] = 1
-        if 0 <= i - k < len(l1) and 0 <= j - k < len(l1):
-            l1[i - k][j - k] = 1
-        if 0 <= i - k < len(l1) and 0 <= j + k < len(l1):
-            l1[i - k][j + k] = 1
-        if 0 <= i + k < len(l1) and 0 <= j - k < len(l1):
-            l1[i + k][j - k] = 1
-    return l1
-
-
-def dfs(i, j, n, l1):
-    l1 = paint(l1, i, j)
-    if i == n - 1:
-        return 1
-    cnt = 0
-    for k in range(n):
-        if l1[i + 1][k] != 1:
-            cnt += dfs(i + 1, k, n, deepcopy(l1))
-    return cnt
-
-
+# 백준 9663 N-Queen -> 백트래킹
+def promising(i, l1):
+  k = 1
+  flag = True
+  while k<i and flag:
+    if l1[i] == l1[k] or abs(l1[i] - l1[k]) == i-k:
+      flag = False
+    k += 1
+  return flag
+def dfs(i, l1):
+  n = len(l1)-1
+  cnt = 0
+  if promising(i, l1):
+    if i == n:
+      global ans
+      ans += 1
+    else:
+      for j in range(1, n+1):
+        l1[i+1] = j
+        dfs(i+1, l1)
 n = int(input())
-
-cnt = 0
-for j in range(n):
-    l1 = [[0 for _ in range(n)] for _ in range(n)]
-    cnt += dfs(0, j, n, l1)
-print(cnt)
+l1 = [0 for _ in range(n+1)]
+ans = 0
+dfs(0, l1)
+print(ans)
