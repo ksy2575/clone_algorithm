fc3f42c968a3de752a494ffa72500b5939787883
SooYong Kim <kst2575@naver.com>
2021-04-03 23:52:09 +0900

    21.04.03 프로그래머스 문제 풀이

--- PROGRAMMERS/PROGRAMMERS dfs-Minimize Sales Decline.py
+++ PROGRAMMERS/PROGRAMMERS dfs-Minimize Sales Decline.py
@@ -1,35 +1,43 @@
 # 프로그래머스 매출 하락 최소화
 from collections import defaultdict
-from copy import deepcopy
 import sys
 saleList = []
 dp = []
-def do(l1, i, set1, pre):
-    temp = 2147483648
-    print(dp)
-    print(set1)
-    if pre != 0 and dp[pre] != -1:
-      return dp[pre]
-    for v in l1[i]:
-      if i == len(l1)-1:
-        dp[v] = saleList[v-1]
-      else:
-        dp[v] = do(l1, i+1, set1|set([v]), v)
-      temp = min(temp, dp[v])
-    return temp+saleList[pre-1]
+def do(dic, curr):
+    if curr not in dic:
+      dp[curr][0] = 0
+      dp[curr][1] = saleList[curr-1]
+      return (dp[curr][0], dp[curr][1])
+
+    if dp[curr][0] == -1:
+      temp = float('inf')
+      sumTemp = 0
+      for v in dic[curr]:
+        sumTemp += do(dic, v)[0]
+      for v in dic[curr]:
+        temp = min(temp, sumTemp-dp[v][0]+dp[v][1])
+      dp[curr][0] = temp
+
+    if dp[curr][1] == -1:
+      temp = 0
+      for v in dic[curr]:
+        temp += min(do(dic, v))
+      dp[curr][1] = saleList[curr-1]+temp
+
+    return (dp[curr][0], dp[curr][1])
 
 def solution(sales, links):
-    sys.setrecursionlimit(10**7)
+    sys.setrecursionlimit(10**8)
     dic = defaultdict(set)
     global saleList, dp
     saleList = sales
-    dp = [0] + [-1]*len(sales)
-    print(dp)
+    dp = [[0, 0]] +[[-1, -1] for _ in range(len(sales))]
     for link in links:
-      dic[link[0]].add(link[0])
       dic[link[0]].add(link[1])
-    print(sales)
-    print(dic)
-
-    return do(list(dic.values()), 0, set(), 0)
+    return min(do(dic, 1))
 print(solution([14, 17, 15, 18, 19, 14, 13, 16, 28, 17], [[10, 8], [1, 9], [9, 7], [5, 4], [1, 5], [5, 10], [10, 6], [1, 3], [10, 2]]))
+print(solution([5, 6, 5, 3, 4], [[2,3], [1,4], [2,5], [1,2]]))
+print(solution([5, 6, 5, 1, 4], [[2,3], [1,4], [2,5], [1,2]]))
+print(solution([10, 10, 1, 1], [[3,2], [4,3], [1,4]]))
+print(solution([0, 0], [[1,2]]))
+print(solution([i for i in range(9000)], [[i,i+1] for i in range(9000)]))
