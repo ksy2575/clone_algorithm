1815177ebd2aae0efa90b7dbb9a19ce16df78602
SooYong Kim <kst2575@naver.com>
2021-04-04 00:03:13 +0900

    21.04.04 프로그래머스 문제 풀이

--- PROGRAMMERS/PROGRAMMERS dp-Minimize Sales Decline.py
+++ PROGRAMMERS/PROGRAMMERS dp-Minimize Sales Decline.py
@@ -10,13 +10,20 @@ def do(dic, curr):
       return (dp[curr][0], dp[curr][1])
 
     if dp[curr][0] == -1:
-      temp = float('inf')
       sumTemp = 0
+      isTrue = False
+      temp = float('inf')
       for v in dic[curr]:
         sumTemp += do(dic, v)[0]
       for v in dic[curr]:
-        temp = min(temp, sumTemp-dp[v][0]+dp[v][1])
-      dp[curr][0] = temp
+        if dp[v][1] <= dp[v][0]:
+          isTrue = True
+          sumTemp = sumTemp-dp[v][0]+dp[v][1]
+        temp = min(temp, dp[v][1] - dp[v][0])
+      if isTrue:
+        dp[curr][0] = sumTemp
+      else:
+        dp[curr][0] = sumTemp + temp
 
     if dp[curr][1] == -1:
       temp = 0
@@ -36,8 +43,3 @@ def solution(sales, links):
       dic[link[0]].add(link[1])
     return min(do(dic, 1))
 print(solution([14, 17, 15, 18, 19, 14, 13, 16, 28, 17], [[10, 8], [1, 9], [9, 7], [5, 4], [1, 5], [5, 10], [10, 6], [1, 3], [10, 2]]))
-print(solution([5, 6, 5, 3, 4], [[2,3], [1,4], [2,5], [1,2]]))
-print(solution([5, 6, 5, 1, 4], [[2,3], [1,4], [2,5], [1,2]]))
-print(solution([10, 10, 1, 1], [[3,2], [4,3], [1,4]]))
-print(solution([0, 0], [[1,2]]))
-print(solution([i for i in range(9000)], [[i,i+1] for i in range(9000)]))
