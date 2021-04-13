479e54f588f287ef892695b8fe35db4b1d83613d
SooYong Kim <kst2575@naver.com>
2021-04-13 22:08:07 +0900

    21.04.13 프로그래머스 문제 풀이

--- PROGRAMMERS/PROGRAMMERS brute force-locks & keys.py
+++ PROGRAMMERS/PROGRAMMERS brute force-locks & keys.py
@@ -18,31 +18,22 @@ def do(key, lock):
     for i in range(len(lock)):
         for j in range(len(lock)):
             lockList[i + len(lock)][j + len(lock)] = lock[i][j]
-    for i in range(start - len(key) + 1, end):
-        for j in range(start - len(key) + 1, end):
+    for i in range(start - len(key), end):
+        for j in range(start - len(key), end):
             if find(key, deepcopy(lockList), i, j, start, end, lock):
-                print(i, j)
                 return True
             else:
                 continue
-            print(i, j)
-            # for ki in range(len(key)):
-            #   for kj in range(len(key)):
-            #     if start <= i+ki < end and start <= j+kj < end:
-            #       lockList[i+ki][i+kj] = lockList[i+ki][i+kj]^key[ki][kj]
     return False
 
 
 def find(key, lockList, i, j, start, end, lock):
     for ki in range(len(key)):
         for kj in range(len(key)):
-            if start <= i + ki < end and start <= j + kj < end:
-                lockList[i + ki][i + kj] = lockList[i + ki][i + kj] ^ key[ki][kj]
-                if lockList[i + ki][i + kj] == 0:
-                    return False
+            lockList[i + ki][j + kj] = lockList[i + ki][j + kj] ^ key[ki][kj]
     for ii in range(len(lock)):
         for jj in range(len(lock)):
-            if lockList[i + len(lock)][j + len(lock)] == 0:
+            if lockList[ii + len(lock)][jj + len(lock)] == 0:
                 return False
 
     return True
@@ -52,11 +43,9 @@ def solution(key, lock):
     keyList = [key]
     for _ in range(3):
         keyList.append(createKey(keyList[-1]))
-    # print(keyList)
     for k in keyList:
-        print(do(k, lock))
-        # break
-    return
-
-
-print(solution([[0, 0, 0], [1, 0, 0], [0, 1, 1]], [[1, 1, 1], [1, 1, 0], [1, 0, 1]]))
+        if do(k, lock):
+            return True
+    return False
+# print(solution([[0, 0, 0], [1, 0, 0], [0, 1, 1]], [[1, 1, 1], [1, 1, 0], [1, 0, 1]]))
+# print(solution([[1, 1], [0, 0]], [[1, 1, 1], [1, 1, 1], [1, 0, 1]]))
