9d3b400262859d1fd02848ee19ec4508b04d6b0f
SooYong Kim <kst2575@naver.com>
2021-04-20 00:01:49 +0900

    21.04.20 프로그래머스 문제 풀이

--- PROGRAMMERS/PROGRAMMERS test-무지의 먹방 라이브.py
+++ PROGRAMMERS/PROGRAMMERS test-무지의 먹방 라이브.py
@@ -1,29 +1,31 @@
-# 프로그래머스 메뉴 리뉴얼
-from itertools import combinations
-def solution(orders, course):
-    answer = []
-    dic = {}
-    tempList = [[0, []]] + [[0, []] for _ in range(course[-1])]
-    for order in orders:
-      l1 = list(order)
-      for num in course:
-        combi = combinations(l1, num)
-        for com in combi:
-          key = "".join(sorted(com))
-          if key in dic:
-            dic[key] += 1
-          else:
-            dic[key] = 1
-          if dic[key] > tempList[len(key)][0]:
-            tempList[len(key)][0] = dic[key]
-            tempList[len(key)][1] = [key]
-          elif dic[key] == tempList[len(key)][0]:
-            tempList[len(key)][1].append(key)
-    for cnt, temps in tempList:
-      if cnt >= 2:
-        for temp in temps:
-          answer.append(temp)
+# 프로그래머스 무지의 먹방 라이브
+def solution(food_times, k):
+    ans = 0
+    left = 0
+    right = 2000000
+    length = len(food_times)
 
-    return sorted(answer)
-print(solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2,3,4]))
-# print(solution(["XYZ", "XWY", "WXA"], [2,3,4]))
+    while left <= right:
+      mid = (left+right)//2
+      time = mid*length
+      for food in food_times:
+        temp = food-mid
+        if temp < 0:
+          time += temp
+      if time <= k:
+        left = mid + 1
+        aroundTime = mid
+      else:
+        right = mid - 1
+    for i in range(length):
+      food_times[i] -= aroundTime
+    ans = k-(aroundTime * length)
+    for i in range(length):
+      if ans == 0:
+        return i
+      if food_times[i] < 0:
+        ans += food_times[i]
+    return -1
+# print(solution([3, 1, 2], 5))
+print(solution([4, 2, 3, 6, 7, 1, 5, 8], 16))
+# print(solution([100000000, 100000000, 100000000], 2 * 10**13))
