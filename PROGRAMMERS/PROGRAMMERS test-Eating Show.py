e11a460872a87e4ef3bf076ad289fba6cef29940
SooYong Kim <kst2575@naver.com>
2021-04-20 21:07:54 +0900

    21.04.20 프로그래머스 문제 풀이

--- PROGRAMMERS/PROGRAMMERS test-Eating Show.py
+++ PROGRAMMERS/PROGRAMMERS test-Eating Show.py
@@ -1,8 +1,8 @@
-# 프로그래머스 무지의 먹방 라이브
+# 프로그래머스 무지의 먹방 라이브 -> 이분 탐색
 def solution(food_times, k):
     ans = 0
     left = 0
-    right = 2000000
+    right = 100000000
     length = len(food_times)
 
     while left <= right:
@@ -15,17 +15,21 @@ def solution(food_times, k):
       if time <= k:
         left = mid + 1
         aroundTime = mid
+        retTime = time
       else:
         right = mid - 1
     for i in range(length):
       food_times[i] -= aroundTime
-    ans = k-(aroundTime * length)
-    for i in range(length):
-      if ans == 0:
+    ans = k-retTime
+    for i in range(length+1):
+      if ans == -1:
         return i
-      if food_times[i] < 0:
-        ans += food_times[i]
-    return -1
+      if i == length:
+        return -1
+      if food_times[i] > 0:
+        ans -= 1
 # print(solution([3, 1, 2], 5))
-print(solution([4, 2, 3, 6, 7, 1, 5, 8], 16))
+# print(solution([1, 1, 1, 1], 3))
+# print(solution([4, 2, 3, 6, 7, 1, 5, 8], 17))
 # print(solution([100000000, 100000000, 100000000], 2 * 10**13))
+# solution([1, 5, 5, 5, 5, 6, 7], 31)
