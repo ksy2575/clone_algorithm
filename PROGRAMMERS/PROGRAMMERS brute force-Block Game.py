6f5fbbff441017421955911300256d846c08d04c
SooYong Kim <kst2575@naver.com>
2021-04-22 22:58:18 +0900

    21.04.22 프로그래머스 문제 풀이

--- PROGRAMMERS/PROGRAMMERS brute force-Block Game.py
+++ PROGRAMMERS/PROGRAMMERS brute force-Block Game.py
@@ -4,7 +4,6 @@ from collections import defaultdict
 
 def findshape(board, i, j):
     num = board[i][j]
-    cnt = 0
     # leftTop, rightBottom = (i, i), (-1, -1)
     block = defaultdict(list)
     black = defaultdict(list)
@@ -13,36 +12,62 @@ def findshape(board, i, j):
             if i + ii >= len(board) or j + jj >= len(board[0]) or j + jj < 0:
                 continue
             if board[i + ii][j + jj] == num:
-                cnt += 1
                 block[i + ii].append(j + jj)
             else:
                 if board[i + ii][j + jj] == 0:
-                    board[i + ii][j + jj] = -1
                     black[i + ii].append(j + jj)
                 else:
                     pass
-    print("")
-    print('num:', num)
-    print(block)
-    print(black)
+    # print("")
+    # print('num:', num)
+    # print(block)
+    # print(black)
     four = [[(0, 0), (2, 3)], [(0, 0), (3, 2)], [(0, -1), (3, 1)], [(0, -2), (2, 1)]]
     for curr in four:
         cnt = 0
         iStart, jStart = i + curr[0][0], j + curr[0][1]
         iEnd, jEnd = i + curr[1][0], j + curr[1][1]
+        flag = True
+        l1 = []
         for ii in range(iStart, iEnd):
             for jj in range(jStart, jEnd):
                 if ii >= len(board) or jj >= len(board[0]) or jj < 0:
-                    continue
+                    flag = False
+                    break
                 if board[ii][jj] == num:
                     cnt += 1
-                if board[ii][jj] not in (num, -1):
-                    continue
-                print("ii", "jj")
-                print(ii, jj)
-                # 이 부분 추가
-    for ii in block:
-        print(ii, block[ii])
+                    l1.append(jj)
+                elif board[ii][jj] not in (num, 0):
+                    flag = False
+                    break
+                elif jj in l1:
+                    flag = False
+                    break
+                # print("ii", "jj")
+                # print(ii, jj)
+
+            if not flag:
+                break
+        if cnt == 4:
+            # print('cnt is 4')
+            for index in range(0, iStart):
+                for jj in range(jStart, jEnd):
+                    if board[index][jj] != 0:
+                        for iii in range(iStart, iEnd):
+                            if board[iii][jj] == 0:
+                                flag = False
+                                break
+                    if not flag:
+                        break
+            if not flag:
+                continue
+            for ii in range(iStart, iEnd):
+                for jj in range(jStart, jEnd):
+                    board[ii][jj] = 0
+            return 1
+    return 0
+    # for ii in block:
+    #   print(ii, block[ii])
 
 
 def solution(board):
@@ -54,12 +79,16 @@ def solution(board):
     for i in range(row):
         for j in range(col):
             if board[i][j] != 0 and board[i][j] not in visited:
-                findshape(board, i, j)
+                answer += findshape(board, i, j)
                 visited.add((board[i][j]))
     return answer
 
 
-print(solution([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                [0, 0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 4, 0, 0, 0], [0, 0, 0, 0, 0, 4, 4, 0, 0, 0],
-                [0, 0, 0, 0, 3, 0, 4, 0, 0, 0], [0, 0, 0, 2, 3, 0, 0, 0, 5, 5], [1, 2, 2, 2, 3, 3, 0, 0, 0, 5],
-                [1, 1, 1, 0, 0, 0, 0, 0, 0, 5]]))
+# print(solution([[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,4,0,0,0],[0,0,0,0,0,4,4,0,0,0],[0,0,0,0,3,0,4,0,0,0],[0,0,0,2,3,0,0,0,5,5],[1,2,2,2,3,3,0,0,0,5],[1,1,1,0,0,0,0,0,0,5]]))
+# print(solution([[0,0,0,0,0,0,0,0,0,0]
+# ,[0,0,0,2,2,0,0,0,0,0]
+# ,[0,0,0,2,1,0,0,0,0,0]
+# ,[0,0,0,2,1,0,0,0,0,0]
+# ,[0,0,0,0,1,1,0,0,0,0]
+# ,[0,0,0,0,0,0,0,0,0,0]]))
+print(solution([[2, 2, 3, 3], [1, 2, 3, 4], [1, 2, 3, 4], [1, 1, 4, 4]]))
