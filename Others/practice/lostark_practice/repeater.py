79812750454f80b08bba9e383708e94bb9d010b8
SooYong Kim <kst2575@naver.com>
2021-05-29 23:59:09 +0900

    21.05.29 파이썬 공부

--- Others/practice/lostark_practice/repeater.py
+++ Others/practice/lostark_practice/repeater.py
@@ -1,7 +1,8 @@
 from lostark_practice import main
 import time
 from collections import deque
-
+# 계속해서 정보를 받아오는 프로그램을 만드려 하였으나, 로스트아크 홈페이지의 거래소 시스템은 실제 매물 상태와 최대 10분의 차이가 난다.
+# 10분에 한 번 정보를 받아오고, 이를 계속 저장하다가 필요할 때 출력하는 방식으로 해보기
 dq = deque()
 for i in range(10):
     main.main()
