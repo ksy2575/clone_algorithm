8bd36eea8defc5475e9ea8826cd0897d5f5230dc
김수용 <kst2575@naver.com>
2021-08-24 21:27:16 +0900

    21.08.24 백준 시뮬레이션 문제 풀이(Java)

---
+++ Java/Simulation/Q_13300.java
@@ -0,0 +1,30 @@
+package baekjoon;
+
+import java.util.Scanner;
+
+public class Q_13300 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int k = sc.nextInt();
+		int ans = 0;
+		int[][] arr = new int [2][6];
+		for (int i = 0; i < n; i++) {
+			int s = sc.nextInt();
+			int y = sc.nextInt();
+			arr[s][y-1]++;
+		}
+		for (int i = 0; i < 2; i++) {
+			for (int j = 0; j < 6; j++) {
+				ans += (arr[i][j]/k);
+				if(arr[i][j]%k > 0) {
+					ans++;
+				}
+			}
+		}
+		System.out.println(ans);
+		
+	}
+
+}
