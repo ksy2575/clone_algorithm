572d4f5fb8771693ec976ca164c9fae4ef337d36
김수용 <kst2575@naver.com>
2022-04-08 23:13:33 +0900

    22.04.08 프로그래머스 Simulation 문제 풀이

---
+++ PROGRAMMERS_Java/Simulation/Solution_없는_숫자_더하기.java
@@ -0,0 +1,16 @@
+package lv2;
+
+//Simulation
+public class Solution_없는_숫자_더하기 {
+	public int solution(int[] numbers) {
+        int answer = 45;
+        for(int i : numbers) {
+        	answer -= i;
+        }
+        return answer;
+    }
+	public static void main(String[] args) {
+		Solution_없는_숫자_더하기 solution = new Solution_없는_숫자_더하기();
+		System.out.println(solution.solution(new int[] {1,2,3,4,6,7,8,0}));
+	}
+}
